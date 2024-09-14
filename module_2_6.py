6404fd255bdae9bb6ea3882d462a3da63a818bc0
User <alchell.kem20041974@gmail.com>
2024-09-14 21:46:37 +0700

    def hard

--- module_2_6.py
+++ module_2_6.py
@@ -21,7 +21,7 @@ def key1():
     ar1 = random.randint(3, 20)  # создаем случайное число
     print(ar1)
     for num_1 in range(1, ar1):  # перебираем первое число пары
-        for num_2 in range(num_1, ar1):  # перебираем второе число пары
+        for num_2 in range(num_1+1, ar1):  # перебираем второе число пары
             if ar1 % (num_1 + num_2) == 0:  # проверяем на деление без остатка
                 password += str(num_1) + str(num_2)  # записываем результат
     print(password)
