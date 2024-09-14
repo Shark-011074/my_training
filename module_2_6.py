36baa84b87ed68b6c4667fdecbeebe6654999ffc
User <alchell.kem20041974@gmail.com>
2024-09-14 20:46:54 +0700

    def

---
+++ module_2_6.py
@@ -0,0 +1,18 @@
+import random
+
+
+def key():
+    password = ''
+    ar1 = random.randint(3, 20)  # создаем случайное число
+    print(ar1)
+    for num_1 in range(1, ar1):  # перебираем первое число пары
+        for num_2 in range(1, ar1):  # перебираем второе число пары
+            if num_2 > num_1:  # при итерации второе число пары всегда БОЛЬШЕ первого
+                if ar1 % (num_1 + num_2) == 0:  # проверяем на деление без остатка
+                    password += str(num_1) + str(num_2)  # записываем результат
+    print(password)
+
+    # return ()
+
+
+key()
