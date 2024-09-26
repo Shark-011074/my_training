98972811cbefccf2514747fddb45b086f1799341
User <alchell.kem20041974@gmail.com>
2024-09-26 23:52:35 +0700

    рекурсия_умножение

--- module_3_5.py
+++ module_3_5.py
@@ -1,15 +1,14 @@
 def get_multiplied_digits(number):
+    if number % 10 == 0:
+        number = int(number / 10)
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number)==1:
+    # print(first)
+    if len(str_number) == 1:
         return first
-    else:
-        first = first * get_multiplied_digits(int(str_number[1:]))
+    first = first * get_multiplied_digits(int(str_number[1:]))
 
     return (first)
 
-print(get_multiplied_digits(40203))
 
-# def print_num(a):
-#     print(a)
-# print_num(010)
+print(get_multiplied_digits(40203))
